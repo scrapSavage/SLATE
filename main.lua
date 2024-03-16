cd9c478d9bd396693f65c075c8eecc37c0d5c57a
scrapSavage <95992852+scrapSavage@users.noreply.github.com>
2024-03-15 20:04:34 -0400

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,6 +1,10 @@
 local gui
 local code_editor
 
+local focused_file = 1
+
+local open_files = {}
+
 window({
 	x=90,
 	y=35,
@@ -10,15 +14,17 @@ window({
 
 menuitem({
 		id=0,
-		label="\^:0038383800387C00 Preferences",
+		label="\^:0f19392121213f00 Open file",
 		action=function()
 		end
 })
 
 function _init()
+	store("/ram/cart/untitled.txt","",{})
+	add(open_files,"/ram/cart/untitled.txt")
 	gui = create_gui()
 	code_editor = gui:attach_text_editor({
-		x=0,y=0,
+		x=0,y=12,
 		width=150,
 		height=200,
 		width_rel=1,
@@ -35,4 +41,7 @@ end
 function _draw()
 	gui:update_all()
 	gui:draw_all()
+	window({
+		title=(open_files[focused_file].." - SLATE")
+	})
 end

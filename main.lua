dfead854ea8a0675736bfb890dc6c3ed0857b8d9
scrapSavage <95992852+scrapSavage@users.noreply.github.com>
2024-03-17 11:39:35 -0400

    Update main.lua

--- main.lua
+++ main.lua
@@ -12,10 +12,10 @@ poke(0x4000, get(fetch("/system/fonts/lil_mono.font"))) -- set font
 local gui
 local code_editor
 
+local m = {x=0,y=0,b=0}
+
 local focused_file = 1
-
 local open_files = {}
-
 local focused = false
 
 window({
@@ -37,7 +37,7 @@ local term_button
 
 menuitem({
 	id=0,
-	label="Help",
+	label="\^:386C603010001000 Help",
 	action=function()
 		local help = fetch("https://raw.githubusercontent.com/scrapSavage/SLATE/main/help.txt")
 		store("help.txt",help,{})
@@ -45,10 +45,11 @@ menuitem({
 		set_active_tab(#open_files)
 	end
 })
-menuitem({
-	id=1,
-	label="Close active tab",
-	action=function()
+
+local bottom_bar
+
+function close_tab(idx)
+	if idx==focused_file then
 		if focused_file-1==0 then
 			if #open_files<2 then
 				exit()
@@ -62,9 +63,13 @@ menuitem({
 			focused_file-=1
 			code_editor:set_text(open_files[focused_file].state)
 		end
+	else
+		deli(open_files,idx)
+		if idx<focused_file then focused_file-=1 end
 	end
-})
+end
 
+local current_pulldown
 function _init()
 	store("/ram/cart/untitled.txt","",{})
 	add(open_files,{path="/ram/cart/untitled.txt",name="untitled.txt",state=fetch("/ram/cart/untitled.txt")})
@@ -75,6 +80,7 @@ function _init()
 		height=200,
 		width_rel=1,
 		height_rel=1,
+		height_add=-25,
 		show_line_numbers=true,
 		syntax_highlighting=true,
 		markup=false,
@@ -138,10 +144,45 @@ function _init()
 		cursor = "pointer",
 		x=32, y=0,
 		width=1000, height=10,
-		tap = function(self)
+		click = function(self)
 			if get_tab_at_mouse() then
-				set_active_tab(get_tab_at_mouse())
+				if m.b==4 then
+					close_tab(get_tab_at_mouse())
+				end
+				if m.b==1 then set_active_tab(get_tab_at_mouse())end
+				if m.b==2 then
+					if current_pulldown then gui:detach(current_pulldown) end
+					local this_tab = get_tab_at_mouse()
+					local x = get_tab_x(this_tab)
+					local pulldown = gui:attach_pulldown({
+						width=64,height=16,y=13
+					})
+					current_pulldown=pulldown
+					pulldown.x = x
+					pulldown:attach_pulldown_item({
+						label="Close tab",
+						action=function()
+							close_tab(this_tab)
+						end
+					})
+				end
 			end
+		end
+	}
+	code_editor.tap=function()
+		if current_pulldown then
+			gui:detach(current_pulldown)
+		end
+	end
+	
+	--bottom bar
+	bottom_bar = gui:attach{
+		x=0,y=32,
+		width=1000, height=14,
+		draw = function(self)
+			rectfill(0,0,1000,18,16)
+			local colnum, rownum = code_editor.get_cursor()
+			print("Col: "..tostr(colnum),2,2,1)
 		end
 	}
 end
@@ -181,6 +222,7 @@ function mouse_aabb(x1,y1,x2,y2)
 end
 
 function _draw()
+	m.x,m.y,m.b = mouse()
 	gui:update_all()
 	gui:draw_all()
 	window({
@@ -198,6 +240,16 @@ function _draw()
 		tab(32+offset,txtw(file.name)+2,tern(foc,-1,0),foc,file.name)
 		offset+=txtw(file.name)+4
 	end
+	bottom_bar.y=get_display():height()-11
+end
+
+function get_tab_x(idx)
+	local x=32
+	for i=1,#open_files do
+		if i==idx then return x end
+		x+=txtw(open_files[i].name)+4
+	end
+	return x
 end
 
 function set_active_tab(idx)
